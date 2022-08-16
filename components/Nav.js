546dd3a1354d0994dc9d07d3ecfdba47d078ffd3
Arnold Omagbemi <arnoldomagbemi@gmail.com>
2022-08-16 13:21:12 +0100

    deploy

---
+++ components/Nav.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { FiShoppingBag } from "react-icons/fi";
+import { NavStyles, NavItems } from "../styles/NavStyles.js"
+import Cart from "./Cart.js";
+import {useStateContext} from "../lib/context"
+import User from "./User.js";
+import { UseUser } from "@auth0/nextjs-auth0";
+
+const { AnimatePresence, motion } = require('framer-motion');
+
+export default function Nav() {
+    const {showCart, setShowCart, totalQuantities} = useStateContext();
+    return (
+        <NavStyles>
+        <Link href={"/"}>Styled.</Link>
+        <NavItems>
+            <User />
+        <div onClick={() => setShowCart(true)}>
+            {totalQuantities > 0 && (
+             <motion.span animate={{ scale: 1 }} initial={{ scale: 0 }}>
+                {totalQuantities}
+             </motion.span>
+             )}
+            <FiShoppingBag />
+            <h3>Cart</h3>
+        </div>
+    </NavItems>
+    <AnimatePresence>
+     {showCart && <Cart />} 
+    </AnimatePresence>
+    </NavStyles>
+    )
+}; 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: { 
+    styledComponents: true,
+  },
   swcMinify: true,
-}
+  async redirects() {
+    return [{ source: "/canceled", destination: "/", permanent: true }]
+  }
+};
 
 module.exports = nextConfig
